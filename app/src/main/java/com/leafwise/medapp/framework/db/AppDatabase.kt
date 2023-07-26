8a79bead387468b398e239a312b89603f28fb643
Davila <joao.davila.001@pucrs.br>
2023-07-26 16:28:38 -0300

    Allowing AutoMigration in DB

--- app/src/main/java/com/leafwise/medapp/framework/db/AppDatabase.kt
+++ app/src/main/java/com/leafwise/medapp/framework/db/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import com.leafwise.medapp.framework.db.dao.MedicationDao
 import com.leafwise.medapp.framework.db.entity.MedicationEntity
 
-@Database(entities = [MedicationEntity::class], version = 1)
+@Database(
+    entities = [MedicationEntity::class],
+    version = 1,
+    exportSchema = true
+)
 
 abstract class AppDatabase : RoomDatabase() {
     abstract fun medicationDao(): MedicationDao
